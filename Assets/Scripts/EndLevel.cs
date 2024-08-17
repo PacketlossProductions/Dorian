bfc3b9eff1a28f7314fc34e9c4b01537ccd06a52
Danté Brans <dante.brans.dev@gmail.com>
2024-08-17 23:49:19 +0200

    Borgir prefab

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // levelID = scene.GetComponent<LevelID>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+                                                // go to a next level
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

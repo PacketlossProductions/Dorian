adc5a3b2694fe67dcf5c47cf95a9f41493dc2a55
Martijn Braam <martijn@brixit.nl>
2024-08-20 01:24:29 +0200

    Add lore

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayButton()
     {
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("StartScene");
     }
 
     public void StartLevelID(int levelID)

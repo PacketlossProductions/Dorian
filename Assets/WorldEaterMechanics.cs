fa7da8061240df1a9786ff3985f1213a2b3bab34
Martijn Braam <martijn@brixit.nl>
2024-08-20 15:51:03 +0200

    Moon yeet

--- Assets/WorldEaterMechanics.cs
+++ Assets/WorldEaterMechanics.cs
@@ -11,6 +11,9 @@ public class WorldEaterMechanics : MonoBehaviour
     Animator animator;
     public float consumptionDistance = 10.0f;
 
+    public Vector2 earth;
+    public Vector2 me;
+
     void Awake()
     {
         animator = GetComponent<Animator>();
@@ -26,13 +29,31 @@ public class WorldEaterMechanics : MonoBehaviour
 
         if(gravityCenter != null)
         {
-            Vector2 earth = new Vector2(gravityCenter.transform.position.x, gravityCenter.transform.position.y);
-            Vector2 me = new Vector2(transform.position.x, transform.position.y);
-            gravity = (me - earth).normalized * -9.81f;
+            earth = new Vector2(gravityCenter.transform.position.x, gravityCenter.transform.position.y);
+            me = new Vector2(transform.position.x, transform.position.y);
+            gravity = (me - earth).normalized * -9f;
             Physics2D.gravity = gravity;
 
-            angle = Vector2.Angle(me, earth);
-            this.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -angle);
+            angle = GetAngle(earth, me);
+            this.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle - 90);
         }
+    }
+
+    public float GetAngle(Vector2 A, Vector2 B)
+    {
+        //difference
+        var Delta = B - A;
+        //use atan2 to get the angle; Atan2 returns radians
+        var angleRadians = Mathf.Atan2(Delta.y, Delta.x);
+
+        //convert to degrees
+        var angleDegrees = angleRadians * Mathf.Rad2Deg;
+
+        //angleDegrees will be in the range (-180,180].
+        //I like normalizing to [0,360) myself, but this is optional..
+        if (angleDegrees < 0)
+            angleDegrees += 360;
+
+        return angleDegrees;
     }
 }

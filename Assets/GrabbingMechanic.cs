daf1787fe70f7f6f72bcc9ab23c349111d99a1e4
Martijn Braam <martijn@brixit.nl>
2024-08-23 17:13:19 +0200

    Correctly calculate the grabbing position

--- Assets/GrabbingMechanic.cs
+++ Assets/GrabbingMechanic.cs
@@ -18,6 +18,7 @@ public class GrabbingMechanic : MonoBehaviour
 
     private GameObject handPosition;
     private float previousScale = 1.0f;
+    private float pickupSelfScale = 1.0f;
     private Rigidbody2D hrb;
 
     private int previousLayer = 0;
@@ -30,11 +31,16 @@ public class GrabbingMechanic : MonoBehaviour
 
     void Update()
     {
-        if(HeldItem != null)
+        if (HeldItem != null)
         {
-            Vector3 grabPos = (handPosition.transform.position - transform.position) * 3;
-            Vector3 offset = grabPos * (HeldItem.transform.localScale.y / 2);
+            Vector3 grabPos = (handPosition.transform.position - transform.position);
+            float itemSize = HeldItem.transform.localScale.y / 4;
+            Vector3 offset = grabPos + new Vector3(transform.localScale.x > 0 ? itemSize : -itemSize, itemSize, 1.0f);
+
             hrb.MovePosition(new Vector2(offset.x + transform.position.x, offset.y + transform.position.y));
+
+            float newScale = transform.localScale.y / pickupSelfScale * previousScale;
+            HeldItem.transform.localScale = new Vector3(newScale, newScale, 1.0f);
         }
     }
 
@@ -44,6 +50,7 @@ public class GrabbingMechanic : MonoBehaviour
         previousLayer = item.layer;
         previousScale = item.transform.localScale.y;
         item.layer = (int)Mathf.Log(heldItemsLayer.value, 2);
+        pickupSelfScale = transform.localScale.y;
         hrb = item.GetComponent<Rigidbody2D>();
         hrb.gravityScale = 0.0f;
         HeldItem = item;
